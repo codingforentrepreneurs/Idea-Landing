ee53a5a12943d163cf05b1d197a90fd4103045aa
CFE <hello@teamcfe.com>
2017-02-21 12:35:03 -0800

    Update production for db

--- ideas/settings/production.py
+++ ideas/settings/production.py
@@ -80,6 +80,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
