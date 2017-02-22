5c995dd9b148846ef7bbe11a3f3baa427f77acfb
CFE <hello@teamcfe.com>
2017-02-21 16:01:16 -0800

    Page model and dynamic homepage

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    title               = models.CharField(max_length=220)
+    title_description   = models.TextField(blank=True, null=True)
+    title_btn           = models.CharField(max_length=50, default='Join')
+    title_btn_url       = models.CharField(max_length=50, blank=True, null=True)
+    content             = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.views.generic import View, FormView, CreateView
 
 from newsletter.forms import JoinForm
-
+from .models import Page
 
 
 # class HomeView(View):
@@ -11,10 +12,19 @@ from newsletter.forms import JoinForm
 
 
 
-class HomeView(CreateView):
+class HomeView(SuccessMessageMixin, CreateView):
     template_name = 'pages/home.html'
     form_class  = JoinForm
     success_url = '/'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context['page_obj'] = Page.objects.all().first() #.order_by("?").first()
+        return context
+
+    def get_success_message(self, cleaned_data):
+        print(cleaned_data)
+        return "Thank you for joining!"
 
     # def form_valid(self, form):
     #     email = form.cleaned_data.get("email")
