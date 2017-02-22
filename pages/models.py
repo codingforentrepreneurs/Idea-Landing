58cd3c3a7057c069d9c3b42d3f76b2f31a5a904b
CFE <hello@teamcfe.com>
2017-02-21 17:36:48 -0800

    Update page layout/format

--- pages/models.py
+++ pages/models.py
@@ -2,12 +2,21 @@ from django.db import models
 
 # Create your models here.
 
+LAYOUT_CHOICES = (
+    ('standard', 'Standard'),
+    ('stacked', 'Stacked'),
+)
+
 class Page(models.Model):
     title               = models.CharField(max_length=220)
     title_description   = models.TextField(blank=True, null=True)
     title_btn           = models.CharField(max_length=50, default='Join')
     title_btn_url       = models.CharField(max_length=50, blank=True, null=True)
     content             = models.TextField(blank=True, null=True)
+    show_nav            = models.BooleanField(default=True)
+    nav_color           = models.CharField(max_length=7, default='#000000')
+    layout              = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default='standard')
+    video_embed         = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
