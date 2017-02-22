7ff9ee60d1f6a69d13a1997594971ebdefbddcbb
CFE <hello@teamcfe.com>
2017-02-22 14:34:15 -0800

    Update featured items

--- pages/models.py
+++ pages/models.py
@@ -25,6 +25,16 @@ class Page(models.Model):
     nav_color           = models.CharField(max_length=7, default='#000000', validators=[layout_validator])
     layout              = models.CharField(max_length=20, choices=LAYOUT_CHOICES, default='standard')
     video_embed         = models.TextField(null=True, blank=True)
+    featured            = models.BooleanField(default=False)
+    active              = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if self.featured:
+            qs = Page.objects.filter(featured=True).exclude(pk=self.pk)
+            if qs.exists():
+                qs.update(featured=False)
+        super(Page, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
