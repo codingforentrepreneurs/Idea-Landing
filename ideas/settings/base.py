1948ff30dd6a1f9af048cfb7abddb944f95901d1
Justin Mitchel <jmitchel3@gmail.com>
2017-02-23 16:39:29 -0800

    Add context processor for project name

--- ideas/settings/base.py
+++ ideas/settings/base.py
@@ -79,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'ideas.context_processors.hiit_project_name',
             ],
         },
     },
